dd80d95d38f0bef67494868b14a71ed73e0cfa87
Kaplaugher <kaplaugher94@gmail.com>
2019-01-10 19:12:36 -0500

    finished vuex

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <Navbar />
+    <NotificationContainer />
     <router-view :key="$route.fullPath" />
   </div>
 </template>
 <script>
 import Navbar from "@/components/Navbar.vue";
+import NotificationContainer from "@/components/NotificationContainer";
 export default {
   name: "App",
   components: {
-    Navbar
+    Navbar,
+    NotificationContainer
   }
 };
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import EventService from '@/services/eventService.js';
 import * as user from '@/store/modules/user.js';
 import * as event from '@/store/modules/event.js';
+import * as notification from '@/store/modules/notification.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     user,
-    event
+    event,
+    notification
   },
   state: {
     categories: [
