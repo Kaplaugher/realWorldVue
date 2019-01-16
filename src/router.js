23f84651231ce16f462c65d66ef95bb3bb6a5b56
Kaplaugher <kaplaugher94@gmail.com>
2019-01-16 17:29:29 -0500

    error pages

--- src/router.js
+++ src/router.js
@@ -5,6 +5,8 @@ import EventList from './views/EventList.vue';
 import EventShow from './views/EventShow.vue';
 import NProgress from 'nprogress';
 import store from '@/store/store';
+import NotFound from './views/NotFound.vue';
+import NetworkIssue from './views/NetworkIssue.vue';
 
 Vue.use(Router);
 
@@ -29,16 +31,44 @@ const router = new Router({
       props: true,
       beforeEnter(routeTo, routeFrom, next) {
         // before this route is loaded
-        store.dispatch('event/fetchEvent', routeTo.params.id).then(event => {
-          routeTo.params.event = event;
-          next();
-        });
+        store
+          .dispatch('event/fetchEvent', routeTo.params.id)
+          .then(event => {
+            routeTo.params.event = event;
+            next();
+          })
+          .catch(error => {
+            if (error.response && error.response.status == 404) {
+              next({
+                name: '404',
+                params: { resource: 'event' }
+              });
+            } else {
+              next({ name: 'network-issue' });
+            }
+          });
       }
     },
     {
       path: '/event/create',
       name: 'event-create',
       component: EventCreate
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: NotFound,
+      props: true
+    },
+    {
+      path: '/network-issue',
+      name: 'network-issue',
+      component: NetworkIssue
+    },
+    {
+      // Here's the new catch all route
+      path: '*',
+      redirect: { name: '404', params: { resource: 'page' } }
     }
   ]
 });
